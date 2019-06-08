d816a37b5393b520dd581ceb687fa8562ce811c3
fish <1976669832@qq.com>
2019-06-08 11:31:08 +0800

    创建表

---
+++ src/main/java/com/example/demo/CreatetableApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatetableApplication.class, args);
+	}
+
+}
